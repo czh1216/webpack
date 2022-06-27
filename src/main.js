e6897526a82a6a700ebf0d270ddbc7295030bfb9
czh <2593620983@qq.com>
2022-06-27 21:07:57 +0800

    固定

--- src/main.js
+++ src/main.js
@@ -5,3 +5,22 @@ import $ from 'jquery'
 $('#swiper').css('background-color', 'pink')
 
 import './styles/index.less'
+
+import './styles/index.css'
+
+import imgUrl from './assets/1.gif';
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
+
+import imgUrl1 from './assets/1.gif'
+const theImg = document.createElement("img")
+theImg.src = imgUrl1
+document.body.appendChild(theImg)
+
+import './assets/fonts/iconfont.css';
+
+class App {
+    static a = 123
+}
+console.log(App.a);
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,39 @@ module.exports ={
             {
                 test:/\.less$/,
                 use:["style-loader", "css-loader","less-loader"],
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/i,
+                use:[
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit:8192,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                type: 'asset', 
+                generator: { 
+                    filename: 'font-[name].[hash:6][ext]'
+                },
+                parser: {
+                    dataUrlCondition: {
+                      maxSize: 1 * 1024,
+                    },
+                },
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'] 
+                    }
+                }
             }
         ]
     }
